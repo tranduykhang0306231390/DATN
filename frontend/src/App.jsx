3862874590394fa89bb9d30db5981534ce3ba3cb
Duy Khang <0306231390@caothang.edu.vn>
2026-06-29 23:05:05 +0700

    update protected rout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,84 +3,70 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import LoginMember from "./pages/auth/LoginMember";
-import LoginStaff from "./pages/auth/LoginStaff";
-import Register from "./pages/auth/Register";
+import LoginStaff  from "./pages/auth/LoginStaff";
+import Register    from "./pages/auth/Register";
 
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import StaffDashboard from "./pages/staff/StaffDashboard";
 
 import MemberLayout from "./layouts/MemberLayout";
-import MemberHome from "./pages/member/MemberHome";
-import Profile from "./pages/member/Profile";
-import Points from "./pages/member/Points";
+import MemberHome   from "./pages/member/MemberHome";
+import Profile      from "./pages/member/Profile";
+import Points       from "./pages/member/Points";
+
+import { MemberRoute, StaffRoute, AdminRoute } from "./routes/ProtectedRoute";
+
 function App() {
     return (
         <BrowserRouter>
-
             <Routes>
 
-                {/* Trang chủ */}
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
+                {/* ── Public ── */}
+                <Route path="/"             element={<Home />} />
+                <Route path="/login"        element={<LoginMember />} />
+                <Route path="/member/login" element={<LoginMember />} />
+                <Route path="/register"     element={<Register />} />
+                <Route path="/staff/login"  element={<LoginStaff />} />
 
-                {/* Khách hàng */}
-                <Route
-                    path="/login"
-                    element={<LoginMember />}
-                />
-
-                <Route
-                    path="/register"
-                    element={<Register />}
-                />
-
-                {/* Staff */}
-                <Route
-                    path="/staff/login"
-                    element={<LoginStaff />}
-                />
-
-                {/* Layout dành cho Member */}
+                {/* ── Vùng MEMBER ── */}
                 <Route
                     path="/member"
-                    element={<MemberLayout />}
+                    element={
+                        <MemberRoute>
+                            <MemberLayout />
+                        </MemberRoute>
+                    }
                 >
-
-                    <Route
-                        index
-                        element={<MemberHome />}
-                    />
-
-                    <Route
-                        path="home"
-                        element={<MemberHome />}
-                    />
-
-                    <Route
-                        path="profile"
-                        element={<Profile />}
-                    />
-                    <Route
-                        path="points"
-                        element={<Points />}
-                    />
+                    <Route index          element={<MemberHome />} />
+                    <Route path="home"    element={<MemberHome />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="points"  element={<Points />} />
+                    {/* Thêm route member ở đây */}
                 </Route>
 
-                {/* Dashboard */}
+                {/* ── Vùng STAFF ── */}
                 <Route
                     path="/staff/dashboard"
-                    element={<StaffDashboard />}
+                    element={
+                        <StaffRoute>
+                            <StaffDashboard />
+                        </StaffRoute>
+                    }
                 />
+                {/* Thêm route staff ở đây */}
 
+                {/* ── Vùng ADMIN ── */}
                 <Route
                     path="/admin/dashboard"
-                    element={<AdminDashboard />}
+                    element={
+                        <AdminRoute>
+                            <AdminDashboard />
+                        </AdminRoute>
+                    }
                 />
+                {/* Thêm route admin ở đây */}
 
             </Routes>
-
         </BrowserRouter>
     );
 }
